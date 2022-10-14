f299d9a98923476a98ba871da5b53ffcb2acdd39
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-10-14 11:17:18 +0200

    styling add new jokes btn

--- src/JokeList.js
+++ src/JokeList.js
@@ -109,7 +109,7 @@ export default class JokeList extends Component {
 						src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
 						alt="icon"></img>
 					<button className="JokeList-getmore" onClick={this.handleClick}>
-						New Jokes
+						Add Jokes
 					</button>
 				</div>
 				<div className="JokeList-jokes">
